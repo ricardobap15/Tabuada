569ba800959750cc93abb94fb0967338f710a75f
ricardobap15 <josersb15@gmail.com>
2020-07-08 00:07:00 -0300

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var tab = document.getElementById('tab')
+
+function tabuada() {
+    var tab = document.getElementById('tab')
+    var num1 = document.getElementById('num')
+    if (num1.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num1.value)
+        var c = 1
+        tab.innerHTML = ''
+        while( c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+            
+            
+        }
+    
+    }
+
+}
+function limpar() {
+   tab.innerHTML = '<option>Digite outro valor</option>'
+}
